30d6c0e6632fcf07d4ffbfc69ef5f0526c35f3b2
Manuel Juan Fosela Águila <manufosela@gmail.com>
2017-03-02 19:15:12 +0100

    split mongo collections to files

---
+++ collections.js
@@ -0,0 +1,4 @@
+Tests 		= new Mongo.Collection('tests');
+Kuestions = new Mongo.Collection('kuestions');
+Answers 	= new Mongo.Collection('answers');
+KTeam 		= new Mongo.Collection('kteam');
---
+++ server/collections.js
@@ -0,0 +1,6 @@
+Results = new Mongo.Collection("results");
+Ranking   = new Mongo.Collection("ranking");
+TimeCounter = new Mongo.Collection("timecounter");
+Active = new Meteor.Collection("active",{connection:null});
+Kcode = new Meteor.Collection("kcode");
+TestsGroup = new Meteor.Collection("testsgroup");
